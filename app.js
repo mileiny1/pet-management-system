a6c5afb1672302599933f05c9b3798bd2f11e6ea
mileiny1 <mileinygreys@gmail.com>
2025-05-01 22:17:34 -0400

    Add app.js

--- app.js
+++ app.js
@@ -27,5 +27,51 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Session setup 
+app.use(session({
+  secret:process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 // 1 day
+  }
+}));
+
+// Flash messages 
+app.use(flash());
+
+// Global variables 
+app.use((req, res, next) => {
+res.locals.success_msg = req.flash('success_msg');
+res.locals.error_msg = req.flash('error_msg');
+res.locals.error = req.flash('error');
+res.locals.user = req.session.user || null;
+next();
+
+});
+
+// Routes
+app.use('/', require('./routes/userRoutes'));
+app.use('/pets', require('./routes/petRoutes'));
+app.use('/food-logs', require('./routes/foodLogRoutes'));
+app.use('/vet-visits', require('./routes/vetVisitRoutes'));
+app.use('/appointments', require('./routes/appointmentRoute'));
+app.use('/medications', require('./routes/medicationRoutes'));
+
+// 404 routes
+app.use((req, res) => {
+  res.status(404).render('404', {
+  little:'404' - 'Page Not Found'
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
 
 
